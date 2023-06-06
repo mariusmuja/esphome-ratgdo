d0c3ae5256ac0b457e8403cb907c8c12ea4243b7
J. Nick Koston <nick@koston.org>
2023-06-05 23:12:02 -0500

    order

--- components/ratgdo/ratgdo.cpp
+++ components/ratgdo/ratgdo.cpp
@@ -96,7 +96,7 @@ namespace ratgdo {
         this->output_gdo_pin_->setup();
         this->input_gdo_pin_->setup();
         this->input_gdo_pin_->pin_mode(gpio::FLAG_INPUT | gpio::FLAG_PULLUP);
-        this->input_obst_pin_->pin_mode(gpio::FLAG_INPUT);
+        this->output_gdo_pin_->pin_mode(gpio::FLAG_OUTPUT);
         this->swSerial.begin(9600, SWSERIAL_8N1, this->input_gdo_pin_->get_pin(), this->output_gdo_pin_->get_pin(), true);
 
         this->pref_ = global_preferences->make_preference<int>(734874333U);
@@ -119,14 +119,14 @@ namespace ratgdo {
         this->store_.trigger_close = this->trigger_close_pin_->to_isr();
         this->store_.trigger_light = this->trigger_light_pin_->to_isr();
 
+        this->input_obst_pin_->pin_mode(gpio::FLAG_INPUT);
+
         this->trigger_open_pin_->pin_mode(gpio::FLAG_INPUT | gpio::FLAG_PULLUP);
         this->trigger_close_pin_->pin_mode(gpio::FLAG_INPUT | gpio::FLAG_PULLUP);
         this->trigger_light_pin_->pin_mode(gpio::FLAG_INPUT | gpio::FLAG_PULLUP);
 
         this->status_door_pin_->pin_mode(gpio::FLAG_OUTPUT);
         this->status_obst_pin_->pin_mode(gpio::FLAG_OUTPUT);
-
-        this->output_gdo_pin_->pin_mode(gpio::FLAG_OUTPUT);
 
         this->trigger_open_pin_->attach_interrupt(RATGDOStore::isrDoorOpen, &this->store_, gpio::INTERRUPT_ANY_EDGE);
         this->trigger_close_pin_->attach_interrupt(RATGDOStore::isrDoorClose, &this->store_, gpio::INTERRUPT_ANY_EDGE);
