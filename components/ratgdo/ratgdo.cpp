08e9ae9dda56307c39f9fe44e4637480cc8304d1
J. Nick Koston <nick@koston.org>
2023-06-05 22:55:30 -0500

    revert to software serial

--- components/ratgdo/ratgdo.cpp
+++ components/ratgdo/ratgdo.cpp
@@ -96,7 +96,7 @@ namespace ratgdo {
         this->output_gdo_pin_->setup();
         this->input_gdo_pin_->setup();
         this->input_gdo_pin_->pin_mode(gpio::FLAG_INPUT | gpio::FLAG_PULLUP);
-        this->input_obst_pin_->pin_mode(gpio::FLAG_INPUT);        
+        this->input_obst_pin_->pin_mode(gpio::FLAG_INPUT);
         this->swSerial.begin(9600, SWSERIAL_8N1, this->input_gdo_pin_->get_pin(), this->output_gdo_pin_->get_pin(), true);
 
         this->pref_ = global_preferences->make_preference<int>(734874333U);
@@ -127,8 +127,6 @@ namespace ratgdo {
         this->status_obst_pin_->pin_mode(gpio::FLAG_OUTPUT);
 
         this->output_gdo_pin_->pin_mode(gpio::FLAG_OUTPUT);
-
-
 
         this->trigger_open_pin_->attach_interrupt(RATGDOStore::isrDoorOpen, &this->store_, gpio::INTERRUPT_ANY_EDGE);
         this->trigger_close_pin_->attach_interrupt(RATGDOStore::isrDoorClose, &this->store_, gpio::INTERRUPT_ANY_EDGE);
