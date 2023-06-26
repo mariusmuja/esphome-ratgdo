b1e06408c0c5f26310d019cfb89a928ad78ac55c
Marius Muja <mariusmuja@gmail.com>
2023-06-26 00:13:54 -0700

    Refactor ratgdo children component state update mechanism

---
+++ components/ratgdo/observable.h
@@ -0,0 +1,50 @@
+#pragma once
+
+namespace esphome {
+namespace ratgdo {
+
+    template <typename T>
+    class observable {
+    public:
+        observable(const T& val)
+            : value(val)
+        {
+        }
+
+        template <typename U>
+        observable& operator=(U value)
+        {
+            if (value != this->value) {
+                this->value = value;
+                this->notify();
+            }
+            return *this;
+        }
+
+        T const* operator&() const { return &this->value; }
+        T* operator&() { return &this->value; }
+
+        T const& operator*() const { return this->value; }
+        T& operator*() { return this->value; }
+
+        template <typename Observer>
+        void subscribe(Observer&& observer)
+        {
+            this->observers.push_back(std::forward<Observer>(observer));
+        }
+
+        void notify()
+        {
+            for (const auto& observer : observers) {
+                observer(value);
+            }
+        }
+
+        T value;
+
+    private:
+        std::vector<std::function<void(T)>> observers;
+    };
+
+} // namespace ratgdo
+} // namespace esphome
